20ba4e650dec93310b598a5168fc92b388462da9
Mohammed-Almuziny <mohammed.abdullah.js@tuwaiq.edu.sa>
2021-12-05 13:42:48 +0300

    create home page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import { Routes, Route } from "react-router";
 import "./App.css";
 
+import { Home } from "./Pages/Home.js";
+
 function App() {
-  return <div className="App">my app</div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
